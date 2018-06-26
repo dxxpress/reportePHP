046dbea921ff1e29a59c41ceac32bc344c02dc05
dxxpress <miguel.cantu@directo-express.com>
2018-06-26 12:32:47 -0500

    holaDeMar

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'Holita de mar, puto el que lo lea 2';
+        echo 'Holita de mar';
         ?>
     </body>
 </html>
